ce4d92cd52af9e0a30bd270771311fe2d8daec02
Kevin McGrevey <niv3kmcg@gmail.com>
2020-06-01 06:14:37 -0600

    Adds Users with zipcodes to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,21 @@ User.create(email: 'user_two@example.com',
 User.create(email: 'user_three@example.com',
             username:'dev.andrewreid',
             flat_id: '5ed2cb06ad6a5566efa7cfbd')
+# for searching
+User.create(email: 'kevin@example.com',
+            username:'kevin_test',
+            flat_id: '5ed08db2948cbd6d6daa972f',
+            zipcode: '80222')
+User.create(email: 'user4@example.com',
+            username:'Denver 1',
+            flat_id: '5ecda42869109757b671ceaa',
+            zipcode: '80210')
+User.create(email: 'user5@example.com',
+            username:'Denver 2',
+            flat_id: '5ecda8f84d6bf46345bcc4c9',
+            zipcode: '80222')
+User.create(email: 'user6@example.com',
+            username:'LA Woman',
+            flat_id: '5ed2cb06ad6a5566efa7cfbd',
+            zipcode: '90001')
 
